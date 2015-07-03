697cc995bdcab578fe51d0ac63d4c8305eb04893
Qix <i.am.qix@gmail.com>
2015-07-03 00:31:44 -0600

    added CORS config

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ function detectConfirmation(description, cb) {
 function PaoServ() {
   this.server = express();
 
+  this.server.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
   this.server.get('/r', function(req, res) {
     res.send(statuses.subreddits);
   });
